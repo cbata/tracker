c6eabc485448e1df25841597be695ee8336fee99
Chris Bata <cjbata@gmail.com>
2020-11-13 05:49:35 -0700

    Add new colorsets Add "Add Proect" and "Add Item" Other refinements

---
+++ Tracker/Binding-OnChange.swift
@@ -0,0 +1,20 @@
+//
+//  Binding-OnChange.swift
+//  Tracker
+//
+//  Created by Chris Bata on 11/10/20.
+//
+
+import SwiftUI
+
+extension Binding {
+    func onChange(_ handler: @escaping () -> Void) -> Binding<Value> {
+        Binding(
+            get: { self.wrappedValue },
+            set: { newValue in
+                self.wrappedValue = newValue
+                handler()
+            }
+        )
+    }
+}
