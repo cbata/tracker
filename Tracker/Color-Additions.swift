de1cbc41969be2dcbd58505d76e77beadce0b05b
Chris Bata <cjbata@gmail.com>
2020-12-02 06:05:29 -0700

    Add Color extension so SwiftUI can show two system grouped background colors from UIKit
    Create Home view that shows open projects, three uncompleted tasks and separately seven more below those.

---
+++ Tracker/Color-Additions.swift
@@ -0,0 +1,13 @@
+//
+//  Color-Additions.swift
+//  Tracker
+//
+//  Created by Chris Bata on 12/1/20.
+//
+
+import SwiftUI
+
+extension Color {
+    static let systemGroupedBackground = Color(UIColor.systemGroupedBackground)
+    static let secondarySystemGroupedBackground = Color(UIColor.secondarySystemGroupedBackground)
+}
